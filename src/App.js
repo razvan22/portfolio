9e2e4452bc8c562c87c8883d6fa5f4f9f273893c
razvan22 <razviy69@gmail.com>
2021-08-17 09:25:06 +0200

    default-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LightsOut from './games/lights-out/LightsOut';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Razvan's Portfolio</h1>
+     <LightsOut />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/games/lights-out/LightsOut.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+class LightsOut extends Component{
+    constructor (props) {
+        super(props);
+        this.state = {}
+    }
+
+    render () {
+        return(
+            <h1>Lights Out</h1>
+        )
+    }
+
+}
+export default LightsOut;

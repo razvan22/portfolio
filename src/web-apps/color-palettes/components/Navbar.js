789565590cf54510cd21b142618ebb068863acb1
Zero <razviy69@gmail.com>
2021-08-31 07:36:45 +0200

    add-cmpleted-navbar

--- src/web-apps/color-palettes/components/Navbar.js
+++ src/web-apps/color-palettes/components/Navbar.js
@@ -1,21 +1,41 @@
+import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import { Select } from "@material-ui/core";
+import { Close } from "@material-ui/icons";
 import React, { Component } from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 
-import '../style/Navbar.css';
-
+import "../style/Navbar.css";
+import { IconButton } from "@material-ui/core";
 
 export default class Navbar extends Component {
 	constructor(props) {
 		super(props);
+		this.state = { format: "hex", open: false };
+		this.handleFormatChange = this.handleFormatChange.bind(this);
+		this.closeSnackbar = this.closeSnackbar.bind(this);
+	}
+
+	handleFormatChange(e) {
+		this.setState({ format: e.target.value, open: true });
+		this.props.handleChange(e.target.value);
+	}
+
+	closeSnackbar() {
+		this.setState({ open: false });
 	}
 
 	render() {
-    const {level, changeLevel} = this.props;
+		const { level, changeLevel } = this.props;
+		const { format } = this.state;
 		return (
 			<nav className="Navbar">
 				<div className="logo">
-					<a href="#">react-color-picker</a>
+					<a href="#">Color Palette</a>
+				</div>
+				<div className="slider-container">
+					<span>Level: {level}</span>
 					<div className="slider">
 						<Slider
 							defaultValue={level}
@@ -26,6 +46,33 @@ export default class Navbar extends Component {
 						/>
 					</div>
 				</div>
+				<div className="select-container">
+					<Select value={format} onChange={this.handleFormatChange}>
+						<MenuItem value="hex">HEX - #ffff </MenuItem>
+						<MenuItem value="rgb">RGB - #rgb(255, 255, 255)</MenuItem>
+						<MenuItem value="rgba">RGBA - #rgba(255, 255, 255, 1.0)</MenuItem>
+					</Select>
+				</div>
+				<Snackbar
+					anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+					open={this.state.open}
+					autoHideDuration={3000}
+					message={<span id="message-id">Format Changed!</span>}
+					ContentProp={{
+						"aria-describedby": "message-id",
+					}}
+          onClose={this.closeSnackbar}
+					action={
+						<IconButton
+							onClick={this.closeSnackbar}
+							color="inherit"
+							key="close"
+							aria-label="close"
+						>
+							<Close />
+						</IconButton>
+					}
+				></Snackbar>
 			</nav>
 		);
 	}
--- src/web-apps/color-palettes/components/Palette.js
+++ src/web-apps/color-palettes/components/Palette.js
@@ -9,24 +9,31 @@ class Palette extends Component {
 		super(props);
 		this.state = {
 			level: 500,
+			format: "hex"
 		};
 		this.changeLevel = this.changeLevel.bind(this);
+		this.changeColorFormat = this.changeColorFormat.bind(this);
 	}
 
 	changeLevel(level) {
 		this.setState({ level });
 	}
 
+	changeColorFormat(value) {
+		this.setState({format: value});
+	}
+
+
 	render() {
 		const { colors } = this.props.palette;
-		const { level } = this.state;
+		const { level, format } = this.state;
 		const colorBoxes = colors[level].map((color) => (
-			<ColorBox key={color.hex} background={color.hex} name={color.name} />
+			<ColorBox key={color.hex} background={color[format]} name={color.name} />
 		));
 
 		return (
 			<div className="Palette">
-				<Navbar lever={level} changeLevel={this.changeLevel} />
+				<Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeColorFormat} />
 				<div className="Palette-colors">{colorBoxes}</div>
 			</div>
 		);

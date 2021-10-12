f69c2bcc57ca60bdafb9af6c9b32e892c6572946
razvan22 <razviy69@gmail.com>
2021-10-12 20:13:22 +0200

    add-aroundTheWorld-project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import AboutMe from "./pages/AboutMe";
 import LightsOut from "./games/lights-out/LightsOut";
 import ColorPalettes from "./web-apps/color-palettes/ColorPalettes";
+import AroundTheWorld from "./web-apps/raound-the-world/AroundTheWorld";
 import ProjectsMetadataProvider from "./assets/contexts/projectsListContext";
 
 import "./App.css";
@@ -13,10 +14,11 @@ function App() {
 	return (
 		<ProjectsMetadataProvider>
 			<Switch>
-				<Route exact path="/" component={HomePage}></Route>
+				{/* <Route exact path="/" component={HomePage}></Route>
 				<Route exact path="/about" component={AboutMe}></Route>
 				<Route exact path="/games/lights-out" component={LightsOut}></Route>
-				<ColorPalettes />
+				<ColorPalettes /> */}
+				<AroundTheWorld />
 			</Switch>
 		</ProjectsMetadataProvider>
 	);
---
+++ src/web-apps/raound-the-world/AroundTheWorld.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AroundTheWorld() {
+  return (
+    <div>
+      <h1>Around The World !!</h1>
+    </div>
+  )
+}
+
+export default AroundTheWorld
